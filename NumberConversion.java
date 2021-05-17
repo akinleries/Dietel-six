9811e51ead6a5f99b4ac209c635adee8755d9d40
lasisi Ezekiel <ezekielakintunde18@gmail.com>
2021-05-17 15:39:48 +0100

    new commit

--- NumberConversion.java
+++ NumberConversion.java
@@ -5,29 +5,31 @@ public class NumberConversion {
 
     public  void convertToBinary(int number){
     String num = Integer.toBinaryString(number);
-        System.out.print(num+" ");
+        System.out.print(num+"               ");
     }
 
     public  void convertToOctal(int number){
     String num = Integer.toOctalString(number);
-        System.out.print(num+" ");
+        System.out.print(num+"               ");
 
 
     }
-    public void convertToHexal (int number){
+    public void convertToHexadecimal (int number){
      String num = Integer.toHexString(number);
-        System.out.print(num+"       ");
+        System.out.print(num);
     }
 
     public  void convert(){
-        System.out.print("  number                        value  ");
+        System.out.printf("%s%15s%18s","binary","octal","Hexadecimal");
+        System.out.println();
+        System.out.println("-------------------------------------------------------------");
+
         for (int num = 1; num <= 256; num++){
             convertToBinary(num);
             convertToOctal(num);
-            convertToHexal(num);
-            if(num % 3 == 0){
+            convertToHexadecimal(num);
+            
                 System.out.println();
-            }
         }
     }
 
